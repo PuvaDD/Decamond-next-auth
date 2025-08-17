f3cfd4af5464f61a3a8083f707b1df726b55f791
PuvaDD <pooyands16@gmail.com>
2025-08-18 02:32:33 +0330

    feat: Added mockup dashboard page structure

--- decamond-next-auth/src/app/dashboard/page.tsx
+++ decamond-next-auth/src/app/dashboard/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { useDashboardContext } from "@/components/contexts/DashboardContext";
+import { isEmpty } from "ramda";
+
+import Image from "next/image";
+import { globalPrefix } from "@/lib/utils/globalUtils";
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  const dashboardCTX = useDashboardContext();
+  const { user } = dashboardCTX;
+
+  const { title, first, last } = user.name;
+  console.log("user = ", user);
+  return (
+    <div>
+      <h1>Welcome to your dashboard</h1>
+      {user && !isEmpty(user) && (
+        <section>
+          <header>User Info:</header>
+          <div>
+            <Image
+              src={user.picture.large}
+              width={250}
+              height={250}
+              alt={`${globalPrefix}profile-picture`}
+            />
+            <div>
+              <label>Full Name:</label>
+              <div>{`${title}${first} ${last}`}</div>
+            </div>
+            <div>
+              <label>Phone:</label>
+              <div>{user.cell}</div>
+            </div>
+          </div>
+        </section>
+      )}
+    </div>
+  );
 }
