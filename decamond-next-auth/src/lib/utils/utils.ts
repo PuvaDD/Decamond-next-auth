d8d0070044ec33b5890bcd2c8709a222469ca7c6
PuvaDD <pooyands16@gmail.com>
2025-08-18 02:05:03 +0330

    feat: Added Auth Cookie props in utils

--- decamond-next-auth/src/lib/utils/utils.ts
+++ decamond-next-auth/src/lib/utils/utils.ts
@@ -9,6 +9,9 @@ export const PHONE_NUMBER_INP_NAME = `${globalPrefix}auth-phone-number`;
 export const PASSWORD_INPUT_NAME = `${globalPrefix}auth-password`;
 export const FAILED_LOGIN_MESSAGE = "Failed To Fetch logged in user info";
 
+export const AUTH_COOKIE_NAME = `${globalPrefix}authenticated-user`;
+export const AUTH_COOKIE_AGE_IN_MILLISECONDS = 10 * 60 * 1000;
+
 export type LoginFormErrors = z.ZodFlattenedError<LoginFormData>;
 export type LoginFormData = z.infer<typeof loginFormSchema>;
 export type LoginResult =
