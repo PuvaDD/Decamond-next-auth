011ab079ffbdded9d280cdbbe8878f109f556e7e
PuvaDD <pooyands16@gmail.com>
2025-08-17 22:06:59 +0330

    refact: Updated login page's functionality

--- decamond-next-auth/src/app/auth/login/page.tsx
+++ decamond-next-auth/src/app/auth/login/page.tsx
@@ -1,39 +1,54 @@
 "use client";
 
-import { PASSWORD_INPUT_NAME, PHONE_NUMBER_INP_NAME } from "@/lib/utils";
+import {
+  LoginResult,
+  PASSWORD_INPUT_NAME,
+  PHONE_NUMBER_INP_NAME,
+} from "@/lib/utils/utils";
 import { useActionState, useEffect, useState } from "react";
 import { Login } from "@/lib/auth/actions";
 
 import Form from "next/form";
 
+const initialState: LoginResult = {
+  success: false,
+  errors: { formErrors: [], fieldErrors: {} },
+};
+
 export default function LoginPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
-  const [state, loginAction, pending] = useActionState(
+  const [authState, loginAction, pending] = useActionState(
     (prevState: any, formData: FormData) => Login(formData),
-    {}
+    initialState
   );
 
   useEffect(() => {
-    console.log("state = ", state);
+    console.log("authState = ", authState);
     console.log("\n\n<========================>\n\n");
-  }, [state]);
+  }, [authState]);
 
   return (
     <Form action={loginAction}>
       <div>Login Page</div>
       <input
+        placeholder="Phone Number"
         name={PHONE_NUMBER_INP_NAME}
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
       <input
+        placeholder="Password"
         name={PASSWORD_INPUT_NAME}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
       <button>Login</button>
-      {state.error && <div>{state.error}</div>}
+      {!authState.success &&
+        authState.errors &&
+        authState.errors.formErrors.length > 0 && (
+          <div>{authState.errors.formErrors[0]}</div>
+        )}
     </Form>
   );
 }
