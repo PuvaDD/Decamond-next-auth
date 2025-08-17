5c1a59f7c940353e423936a8fa5e9946ee678f54
PuvaDD <pooyands16@gmail.com>
2025-08-18 02:43:06 +0330

    feat: Added metadata

--- decamond-next-auth/src/app/auth/layout.tsx
+++ decamond-next-auth/src/app/auth/layout.tsx
@@ -1,7 +1,19 @@
 import { AUTH_COOKIE_NAME } from "@/lib/utils/authPageUtils";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
 import { cookies } from "next/headers";
+
+export const metadata: Metadata = {
+  title: "Login - Decamond Next.js auth interview project",
+  description: "Login to access your dashboard and manage your account",
+  openGraph: {
+    title: "Login - Decamond Next.js auth",
+    description: "Login to access your dashboard and manage your account",
+    url: "https://decamond.com/auth/login",
+    type: "website",
+  },
+};
 
 export default async function AuthPageLayout({
   children,
