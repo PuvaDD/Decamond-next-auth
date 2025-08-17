09b451a4b4b3038dac04e1b2007e55d04e119498
PuvaDD <pooyands16@gmail.com>
2025-08-17 18:05:28 +0330

    refact: Updated Login page

--- decamond-next-auth/src/app/auth/login/page.tsx
+++ decamond-next-auth/src/app/auth/login/page.tsx
@@ -1,21 +1,39 @@
 "use client";
 
-import { useState } from "react";
-import { Login } from "@/auth/actions";
+import { PASSWORD_INPUT_NAME, PHONE_NUMBER_INP_NAME } from "@/lib/utils";
+import { useActionState, useEffect, useState } from "react";
+import { Login } from "@/lib/auth/actions";
+
 import Form from "next/form";
 
 export default function LoginPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [state, loginAction, pending] = useActionState(
+    (prevState: any, formData: FormData) => Login(formData),
+    {}
+  );
+
+  useEffect(() => {
+    console.log("state = ", state);
+    console.log("\n\n<========================>\n\n");
+  }, [state]);
 
   return (
-    <Form action={Login}>
+    <Form action={loginAction}>
       <div>Login Page</div>
       <input
-        name="phone-number"
+        name={PHONE_NUMBER_INP_NAME}
         value={phoneNumber}
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
+      <input
+        name={PASSWORD_INPUT_NAME}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button>Login</button>
+      {state.error && <div>{state.error}</div>}
     </Form>
   );
 }
