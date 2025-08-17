b7627843eaa0062ce17fcc8df2b24bc4d1e1822b
PuvaDD <pooyands16@gmail.com>
2025-08-18 02:07:33 +0330

    feat: Prevented unauthenticated users from visiting dashboard subdirectories

---
+++ decamond-next-auth/src/app/dashboard/layout.tsx
@@ -0,0 +1,24 @@
+import { DashboardProvider } from "@/components/contexts/DashboardContext";
+import { AUTH_COOKIE_NAME } from "@/lib/utils/utils";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const pushToLogin = () => redirect("/auth/login");
+
+  const cookieStore = await cookies();
+  const authCookie = cookieStore.get(AUTH_COOKIE_NAME);
+
+  if (!authCookie) return pushToLogin();
+
+  const parsedUserInfo = JSON.parse(authCookie.value);
+
+  return (
+    <DashboardProvider user={parsedUserInfo.user}>{children}</DashboardProvider>
+  );
+}
