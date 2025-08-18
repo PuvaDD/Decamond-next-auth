6babe342942ebd61afb581136b7ddccade423f61
PuvaDD <pooyands16@gmail.com>
2025-08-18 13:08:15 +0330

    feat: Dashboard styles

--- decamond-next-auth/src/app/dashboard/page.tsx
+++ decamond-next-auth/src/app/dashboard/page.tsx
@@ -5,34 +5,35 @@ import { globalPrefix } from "@/lib/utils/globalUtils";
 import { isEmpty } from "ramda";
 
 import Image from "next/image";
+import styles from "./dashboard.module.scss";
 
 export default function DashboardPage() {
   const dashboardCTX = useDashboardContext();
   const { user } = dashboardCTX;
 
   const { title, first, last } = user.name;
-  console.log("user = ", user);
+
   return (
-    <div>
+    <div className={styles["wrapper"]}>
       <h1>Welcome to your dashboard</h1>
+
       {user && !isEmpty(user) && (
         <section>
-          <header>User Info:</header>
+          <Image
+            src={user.picture.large}
+            width={250}
+            height={250}
+            alt={`${globalPrefix}profile-picture`}
+          />
+
           <div>
-            <Image
-              src={user.picture.large}
-              width={250}
-              height={250}
-              alt={`${globalPrefix}profile-picture`}
-            />
-            <div>
-              <label>Full Name:</label>
-              <div>{`${title}${first} ${last}`}</div>
-            </div>
-            <div>
-              <label>Phone:</label>
-              <div>{user.cell}</div>
-            </div>
+            <label>Full Name:</label>
+            <div>{`${title}.${first} ${last}`}</div>
+          </div>
+
+          <div>
+            <label>Phone:</label>
+            <div>{user.cell}</div>
           </div>
         </section>
       )}
