264d93e77086cd7cc3b5ee08b1a3063848f78041
PuvaDD <pooyands16@gmail.com>
2025-08-18 14:46:12 +0330

    feat: Conditional nav link display

--- decamond-next-auth/src/app/page.tsx
+++ decamond-next-auth/src/app/page.tsx
@@ -1,17 +1,38 @@
-import Image from "next/image";
+import { AUTH_COOKIE_NAME } from "@/lib/utils/authPageUtils";
+import { isEmpty, isNil } from "ramda";
+import { cookies } from "next/headers";
+
 import styles from "./page.module.scss";
+import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const authCookie = cookieStore.get(AUTH_COOKIE_NAME);
+  let parsedUserInfo;
+
+  try {
+    parsedUserInfo = authCookie && JSON.parse(authCookie.value);
+  } catch (error) {
+    console.log("err = ", error);
+  }
+  const user = parsedUserInfo?.user;
+
+  const userExists = parsedUserInfo && !isNil(user.id) && !isEmpty(user.id);
+
   return (
     <div className={styles.page}>
       <header className={styles["lp-header"]}>
-        <Link href={"/auth/login"} prefetch={true}>
-          Login
-        </Link>
-        <Link href={"/dashboard"} prefetch={true}>
-          Dashboard
-        </Link>
+        {!userExists && (
+          <Link href={"/auth/login"} prefetch={true}>
+            Login
+          </Link>
+        )}
+        {userExists && (
+          <Link href={"/dashboard"} prefetch={true}>
+            Dashboard
+          </Link>
+        )}
       </header>
       <main className={styles.main}>
         <Image
